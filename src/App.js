45b0ac5223349166a326f3b30a29d08142c0ed8c
RudroMajumder <majumder.rudrofficial@gmail.com>
2021-04-15 00:54:11 +0600

    header done

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home/Home';
+
+function App() {
+  return (
+    <div >
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
